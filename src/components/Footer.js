3d53bce0adb45165a9577adf1fd9137c35f4ebd1
Prudhvi Raj <prudhvi@123.com>
2022-12-06 09:11:43 +0530

    footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import SideDrawer from "./SideDrawer";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <SideDrawer />
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Mails</a>
+          </li>
+          <li>
+            <a href="#">Alarms</a>
+          </li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+        </ul>
+      </nav>
+      <SideDrawer />
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
