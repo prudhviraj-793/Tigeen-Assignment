aa4a1ce7e8517d9b27ea247c16934a9c7ca57d89
Prudhvi Raj <prudhvi@123.com>
2022-12-06 08:50:53 +0530

    header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import SideDrawer from "./SideDrawer";
+import logo from "../logo.png";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div>
+        <SideDrawer />
+        <img src={logo} alt="Logo" />
+        <SideDrawer />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">TAB ONE</a>
+          </li>
+          <li>
+            <a href="#">TAB TWO</a>
+          </li>
+          <li>
+            <a href="#">TAB THREE</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
